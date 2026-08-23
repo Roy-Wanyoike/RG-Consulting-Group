1e5cb313e4a8a7b32128203e107c8197acfee50f
Alex Githinji <alexgithinji44@gmail.com~>
2026-08-23 17:10:55 +0300

    updated the favicon

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,6 +3,6 @@ import Image from "next/image";
 
 export function Logo({ inverse = false }: { inverse?: boolean }) {
   return <Link href="/#home" className={inverse ? "logo logo--inverse" : "logo"} aria-label="RG Consulting home">
-    <Image className="logo-image" src="/images/rg-consulting-logo.png" alt="" width={1400} height={1121} priority={!inverse} />
+    <Image className="logo-image" src="/images/rg-consulting-logo-transparent.png" alt="" width={1536} height={1024} priority={!inverse} />
   </Link>;
 }
